a5f412b46b66ead14213b4c83c20ff8bd0867958
noritaketakamichi <noritaket28@gmail.com>
2020-09-29 18:15:11 +0900

    orange ball

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //穴に落ちたら最初に戻す
+        if(transform.position.y<20){
+            BackToStart();
+        };
+    }
+
+    //最初に戻す関数
+    void BackToStart()
+    {
+        SceneManager.LoadScene (0);
+    }
+}
--- Assets/Scripts/CarDrive.cs
+++ Assets/Scripts/CarDrive.cs
@@ -16,7 +16,7 @@ public class CarDrive : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb= GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -24,35 +24,53 @@ public class CarDrive : MonoBehaviour
     {
         Move();
         Turn();
-        Fall();       
+        Fall();
+        CheckFall();
     }
 
     //前後の移動
-    void Move(){
-        if(Input.GetKey("up")){
+    void Move()
+    {
+        if (Input.GetKey("up"))
+        {
             Debug.Log("WWWWWWWWWWWWWWW");
-            rb.AddRelativeForce(new Vector3(Vector3.forward.x,0,Vector3.forward.z)*speed*10);
+            rb.AddRelativeForce(new Vector3(Vector3.forward.x, 0, Vector3.forward.z) * speed * 10);
         }
-        else if(Input.GetKey("down")){
-            rb.AddRelativeForce(new Vector3(Vector3.forward.x,0,Vector3.forward.z)*-speed*10);
+        else if (Input.GetKey("down"))
+        {
+            rb.AddRelativeForce(new Vector3(Vector3.forward.x, 0, Vector3.forward.z) * -speed * 10);
         }
-        Vector3 localVelocity= transform.InverseTransformDirection(rb.velocity);
-        localVelocity.x=0;
-        rb.velocity=transform.TransformDirection(localVelocity);
+        Vector3 localVelocity = transform.InverseTransformDirection(rb.velocity);
+        localVelocity.x = 0;
+        rb.velocity = transform.TransformDirection(localVelocity);
     }
 
     //y軸中心の回転
-    void Turn(){
-        if(Input.GetKey("right")){
-            rb.AddTorque(Vector3.up*turnSpeed);
+    void Turn()
+    {
+        if (Input.GetKey("right"))
+        {
+            rb.AddTorque(Vector3.up * turnSpeed);
         }
-        else if(Input.GetKey("left")){
-            rb.AddTorque(-Vector3.up*turnSpeed);
+        else if (Input.GetKey("left"))
+        {
+            rb.AddTorque(-Vector3.up * turnSpeed);
         }
     }
 
     //重力をかける
-    void Fall(){
-        rb.AddForce(Vector3.down * gravityMultiplier*10);
+    void Fall()
+    {
+        rb.AddForce(Vector3.down * gravityMultiplier * 10);
+    }
+
+    void CheckFall()
+    {
+        Debug.Log(transform.position.y);
+        //穴に落ちたら最初に戻す
+        if (transform.position.y < 20)
+        {
+            transform.position = new Vector3(0,50,0);
+        }
     }
 }
